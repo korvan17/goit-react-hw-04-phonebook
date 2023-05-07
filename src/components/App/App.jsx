a5fe3fcb937b4efbd7c17d26d35342c36ddd76e9
Ivan Kornienko <ivan@homeds.pro>
2023-05-07 21:47:43 +0300

    add contact

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,6 +13,7 @@ export class App extends Component {
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-5', name: 'Ivan Kornienko', number: '551-14-33' },
     ],
     filter: [],
   };
