e80878a8aa9b23de8d978fee92feb258ab655467
Ivan Kornienko <ivan@homeds.pro>
2023-05-07 09:32:51 +0300

    add components

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function ContactList() {
+  return (
+    <div>
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+    </div>
+  );
+}
